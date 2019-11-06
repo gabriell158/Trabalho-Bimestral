070dc6fd3557a4224c96f2e87e46de97c3650c78
gabriell158 <gabripampuch@hotmail.com>
2019-11-06 17:44:19 -0300

    Aprimoramentos
    Mudanças estéticas.

--- trabalho2bi.c
+++ trabalho2bi.c
@@ -13,47 +13,56 @@ typedef struct Aluno {
 
 void cadastrar(TAluno estudantes[],int posicao) {
 
-    int ok;
+    int ok,onze;
 
     system("cls");
     do {
-        printf("Entre com o nome completo:\n");
+        printf("\nEntre com o nome completo:\n");
         fgets(estudantes[posicao].nome, MAX_STRING-1, stdin);
         fgets(estudantes[posicao].nome, MAX_STRING-1, stdin);
-        printf("\nNome: %s\n\n",estudantes[posicao].nome);
-        printf("Seu nome está correto?\n");
-        printf("1 --> Sim.\n2 --> Não.\n");
+        printf("\n\nNome: %s\n\n",estudantes[posicao].nome);
+        printf("\nSeu nome está correto?");
+        printf("\n1 --> Sim.\n2 --> Não.\n");
         scanf("%i",&ok);
         if(ok == 1) {
-            printf("Nome cadastrado com sucesso.\n\n");
+            printf("\nNome cadastrado com sucesso.");
         }
         else {
-            printf("\n\nTente novamente.\n\n");
+            printf("\nTente novamente.");
         }
     }while(ok!=1);
     ok=0;
     do {
-        printf("Entre com seu CPF:\n");
+        printf("\nEntre com seu CPF(apenas números):\n");
         fgets(estudantes[posicao].cpf, MAX_STRING-1, stdin);
         fgets(estudantes[posicao].cpf, MAX_STRING-1, stdin);
-        printf("\nCPF: %s\n\n",estudantes[posicao].cpf);
-        printf("Seu CPF está correto?\n");
-        printf("1 --> Sim.\n2 --> Não.\n");
-        scanf("%i",&ok);
-        if(ok == 1) {
-            printf("CPF cadastrado com sucesso.\n\n");
+        onze = strlen(estudantes[posicao].cpf);
+        if(onze != 12) {
+            printf("CPF inválido!");
         }
         else {
-            printf("\n\nTente novamente.\n\n");
+            printf("\nCPF: %c%c%c.%c%c%c.%c%c%c-%c%c\n",estudantes[posicao].cpf[0],estudantes[posicao].cpf[1],estudantes[posicao].cpf[2],
+            estudantes[posicao].cpf[3],estudantes[posicao].cpf[4],estudantes[posicao].cpf[5],estudantes[posicao].cpf[6],
+            estudantes[posicao].cpf[7],estudantes[posicao].cpf[8],estudantes[posicao].cpf[9],estudantes[posicao].cpf[10]);
+            printf("\nSeu CPF está correto?");
+            printf("\n1 --> Sim.\n2 --> Não.\n");
+            scanf("%i",&ok);
+            if(ok == 1) {
+                printf("\nCPF cadastrado com sucesso.");
+            }
+            else {
+                printf("\nTente novamente.");
+            }
         }
+
     }while(ok!=1);
-    printf("Entre com a sua data de nascimento.\n\n");
+    printf("\nEntre com a sua data de nascimento.");
     ok=0;
     do {
-        printf("Entre com o dia:\n");
+        printf("\nEntre com o dia:\n");
         scanf("%i", &estudantes[posicao].dia);
         if((estudantes[posicao].dia > 31) || (estudantes[posicao].dia < 1)) {
-            printf("Dia inválido!\n");
+            printf("\nDia inválido!");
         }
         else {
             ok = 1;
@@ -61,10 +70,10 @@ void cadastrar(TAluno estudantes[],int posicao) {
     }while(ok!=1);
     ok=0;
     do {
-        printf("Entre com o mes:\n");
+        printf("\nEntre com o mes:\n");
         scanf("%i", &estudantes[posicao].mes);
         if((estudantes[posicao].mes > 12) || (estudantes[posicao].mes < 1)) {
-            printf("Mês inválido!\n");
+            printf("\nMês inválido!");
         }
         else {
             ok = 1;
@@ -72,22 +81,24 @@ void cadastrar(TAluno estudantes[],int posicao) {
     }while(ok!=1);
     ok=0;
     do {
-        printf("Entre com o ano:\n");
+        printf("\nEntre com o ano:\n");
         scanf("%i", &estudantes[posicao].ano);
         if(estudantes[posicao].ano < 1) {
-            printf("Ano inválido!\n");
+            printf("\nAno inválido!");
         }
         else {
             ok = 1;
         }
     }while(ok!=1);
-    printf("Data de nascimento cadastrada com sucesso.\n");
+    printf("\nData de nascimento cadastrada com sucesso.");
     estudantes[posicao].id = posicao;
     estudantes[posicao].situacao = 1;
-    printf("Cadastro finalizado.\n\n");
-    printf("Nome: %s\n",estudantes[posicao].nome);
-    printf("CPF: %s\n",estudantes[posicao].cpf);
-    printf("Data de nascimento: %i/%i/%i\n",estudantes[posicao].dia,estudantes[posicao].mes,estudantes[posicao].ano);
+    printf("\nCadastro finalizado.");
+    printf("\nNome: %s",estudantes[posicao].nome);
+    printf("\nCPF: %c%c%c.%c%c%c.%c%c%c-%c%c\n",estudantes[posicao].cpf[0],estudantes[posicao].cpf[1],estudantes[posicao].cpf[2],
+    estudantes[posicao].cpf[3],estudantes[posicao].cpf[4],estudantes[posicao].cpf[5],estudantes[posicao].cpf[6],
+    estudantes[posicao].cpf[7],estudantes[posicao].cpf[8],estudantes[posicao].cpf[9],estudantes[posicao].cpf[10]);
+    printf("\nData de nascimento: %i/%i/%i",estudantes[posicao].dia,estudantes[posicao].mes,estudantes[posicao].ano);
 }
 
 int listar(TAluno estudantes[],int posicao) {
@@ -96,34 +107,47 @@ int listar(TAluno estudantes[],int posicao) {
 
     for(i=1;i<=posicao;i++) {
         if((estudantes[i].situacao) == 1) {
-            printf("ID: %i\n",estudantes[i].id);
-            printf("Nome: %s\n",estudantes[i].nome);
-            printf("CPF: %s\n",estudantes[i].cpf);
-            printf("Data de nascimento: %i/%i/%i\n",estudantes[i].dia,estudantes[i].mes,estudantes[i].ano);
+            printf("\nID: %i",estudantes[i].id);
+            printf("\nNome: %s",estudantes[i].nome);
+            printf("\nCPF: %c%c%c.%c%c%c.%c%c%c-%c%c\n",estudantes[i].cpf[0],estudantes[i].cpf[1],estudantes[i].cpf[2],
+            estudantes[i].cpf[3],estudantes[i].cpf[4],estudantes[i].cpf[5],estudantes[i].cpf[6],
+            estudantes[i].cpf[7],estudantes[i].cpf[8],estudantes[i].cpf[9],estudantes[i].cpf[10]);
+            printf("\nData de nascimento: %i/%i/%i",estudantes[i].dia,estudantes[i].mes,estudantes[i].ano);
         }
-
     }
 }
 
 void pesquisar(TAluno estudantes[],int posicao) {
 
-    int i;
+    int i,existe=0,ok=0;
     char busca[MAX_STRING];
 
-    system("cls");
-    printf("Digite o nome do estudante cujo cadastro deseja localizar: \n\n");
-    fgets(busca,MAX_STRING-1,stdin);
-    fgets(busca,MAX_STRING-1,stdin);
-    for(i=1;i<=posicao;i++) {
-        if((estudantes[i].situacao) == 1) {
-            if(strcmp(busca,estudantes[i].nome) == 0) {
-                printf("\n\nID: %i\n\n",estudantes[i].id);
-                printf("Nome: %s\n",estudantes[i].nome);
-                printf("CPF: %s\n",estudantes[i].cpf);
-                printf("Data de nascimento: %i/%i/%i\n",estudantes[i].dia,estudantes[i].mes,estudantes[i].ano);
+    do {
+        system("cls");
+        printf("\nDigite o nome do estudante cujo cadastro deseja localizar: \n\n");
+        fgets(busca,MAX_STRING-1,stdin);
+        fgets(busca,MAX_STRING-1,stdin);
+        for(i=1;i<=posicao;i++) {
+            if((estudantes[i].situacao) == 1) {
+                if(strcmp(busca,estudantes[i].nome) == 0) {
+                    printf("\n\nID: %i\n\n",estudantes[i].id);
+                    printf("\nNome: %s",estudantes[i].nome);
+                    printf("\nCPF: %c%c%c.%c%c%c.%c%c%c-%c%c\n",estudantes[i].cpf[0],estudantes[i].cpf[1],estudantes[i].cpf[2],
+                    estudantes[i].cpf[3],estudantes[i].cpf[4],estudantes[i].cpf[5],estudantes[i].cpf[6],
+                    estudantes[i].cpf[7],estudantes[i].cpf[8],estudantes[i].cpf[9],estudantes[i].cpf[10]);
+                    printf("\nData de nascimento: %i/%i/%i",estudantes[i].dia,estudantes[i].mes,estudantes[i].ano);
+                    existe = 1;
+                }
             }
         }
-    }
+        if(existe == 0) {
+            printf("\nUsuário não encontrado.");
+        }
+            printf("\nDeseja pesquisar novamente?");
+            printf("\n1 --> Não.\n2 --> Sim.\n");
+            scanf("%i",&ok);
+    }while(ok!=1);
+
 }
 
 void atualizar(TAluno estudantes[],int posicao) {
@@ -132,7 +156,7 @@ void atualizar(TAluno estudantes[],int posicao) {
 
     do {
         system("cls");
-        printf("\nInforme a ID do usuário que deseja alterar: ");
+        printf("\nInforme a ID do usuário que deseja alterar: \n");
         scanf("%i",&id);
         for(i=0;i<posicao;i++) {
             if(id == estudantes[i].id) {
@@ -155,10 +179,10 @@ void remover(TAluno estudantes[]) {
     int id;
 
     system("cls");
-    printf("\nDigite a ID do usuário que deseja remover: ");
+    printf("\nDigite a ID do usuário que deseja remover: \n\n");
     scanf("%i",&id);
     estudantes[id].situacao = 0;
-    printf("\nUsuário desativado.");
+    printf("\n\nUsuário desativado.");
 
 }
 
@@ -170,11 +194,11 @@ int main() {
 
     printf("\n\n\nCadastro de pessoas.\n\n\n");
     do {
-        printf("\n1 --> Cadastrar.\n");
-        printf("\n2 --> Listar.\n");
-        printf("\n3 --> Pesquisar.\n");
-        printf("\n4 --> Atualizar.\n");
-        printf("\n5 --> Remover.\n");
+        printf("\n1 --> Cadastrar.");
+        printf("\n2 --> Listar.");
+        printf("\n3 --> Pesquisar.");
+        printf("\n4 --> Atualizar.");
+        printf("\n5 --> Remover.");
         printf("\n6 --> Sair.\n\n\n");
         scanf("%i",&opcao);
         switch(opcao) {
